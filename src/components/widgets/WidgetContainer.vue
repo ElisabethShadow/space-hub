62f474c493df3be5d2c1abc1d8efefcdf2df1f56
Elisabeth Schaduw <ecaschaduw@gmail.com>
2024-01-30 17:32:10 +0100

    Add WidgetContainer component with basic styles

---
+++ src/components/widgets/WidgetContainer.vue
@@ -0,0 +1,41 @@
+<template>
+  <!-- Widget Container component -->
+  <div class="widget-container"></div>
+  <div class="header">
+    <h1 class=""></h1>
+    <p class="subtitle"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WidgetContainer",
+};
+</script>
+
+<style lang="scss">
+.widget-container {
+  /* Widget container styles */
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  .header {
+    /* Header styles */
+    .title {
+      /* Title styles */
+      text-align: left;
+      font-size: 24px;
+      font-weight: bold;
+      color: #333333;
+    }
+
+    .subtitle {
+      /* Subtitle styles */
+      text-align: left;
+      font-size: 18px;
+      color: #666666;
+    }
+  }
+}
+</style>
