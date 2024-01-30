2781e536752c4bdf747e7e54bd5ca3ecfd001ca1
Elisabeth Schaduw <ecaschaduw@gmail.com>
2024-01-30 21:12:16 +0100

    Add Sidebar module component and implemented in App

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,44 @@
 <template>
-  <Dashboard />
+  <div id="app" class="app-container">
+    <Sidebar />
+    <div class="main-content">
+      <Dashboard />
+    </div>
+  </div>
 </template>
 
 <script>
 import Dashboard from "@/modules/Dashboard.vue";
+import Sidebar from "@/modules/Sidebar.vue";
 
 export default {
   name: 'App',
   components: {
-    Dashboard
+    Dashboard,
+    Sidebar
   }
 }
 </script>
 
-<style>
-#app {
+<style lang="scss">
+.app-container {
+  display: flex;
+  min-height: 100vh; /* Full viewport height */
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+}
+
+
+
+.sidebar {
+  width: 250px; // Adjust based on your design
+  // Additional sidebar styles
+}
+
+.main-content {
+  flex-grow: 1;
+  text-align: left; // Assuming main content is not centered
   color: #2c3e50;
-  margin-top: 60px;
+  // Additional main content styles
 }
 </style>
+
---
+++ src/modules/Sidebar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="sidebar">
+    <!-- Loop through items and render them -->
+    <div v-for="item in sidebarItems" :key="item.name" class="sidebar-item">
+      <img :src="item.icon" :alt="item.name" class="icon">
+      <span>{{ item.name }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SideBar',
+  data() {
+    return {
+      sidebarItems: [
+        { name: 'Weather', icon: 'path/to/weather-icon.svg' },
+        { name: 'To Do', icon: 'path/to/todo-icon.svg' },
+        // Add other items similarly
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped lang="scss">
+
+@import "../assets/styles/styles.scss";
+
+</style>
